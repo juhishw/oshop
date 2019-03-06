27abee47cfcd8b2b1fb8591b1164472a30b1c548
Juhi Shaw <juhi.shaw@quovantis.com>
2019-03-05 17:30:44 -0800

    Before data table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,24 @@
-import { UserService } from './user.service';
-import { Router } from '@angular/router';
-import { AuthService } from './auth.service';
-import { Component } from '@angular/core';
+import { UserService } from "./user.service";
+import { Router } from "@angular/router";
+import { AuthService } from "./auth.service";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  constructor(private userService: UserService, private auth: AuthService, router: Router) {
+  constructor(
+    private userService: UserService,
+    private auth: AuthService,
+    router: Router
+  ) {
     auth.user$.subscribe(user => {
       if (user) {
         userService.save(user);
 
-        let returnUrl = localStorage.getItem('returnUrl');
+        let returnUrl = localStorage.getItem("returnUrl");
         router.navigateByUrl(returnUrl);
       }
     });
